5ac2697935d60f24e4568133ece63f6d8f9e6057
Ayan Mansuri (Ghost_.) <mansuriayan243@gmail.com>
2026-07-12 06:55:19 +0000

    modified:   hacakathon_env/AssetFlow/AssetFlowApp/urls.py

--- hacakathon_env/AssetFlow/AssetFlowApp/urls.py
+++ hacakathon_env/AssetFlow/AssetFlowApp/urls.py
@@ -18,11 +18,12 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+    path('', views.login_view, name='home'),
     path('register/', views.register_view, name='register'),
     path('otp/', views.otp_verify_view, name='otp_verify'),
     path('', views.login_view, name='login'),
     path('logout/', views.logout_view, name='logout'),
     path('forgot-password/', views.forgot_password_view, name='forgot_password'),
     path('dashboard/', views.dashboard_view, name='dashboard'),
-    path('', views.login_view, name='home'),
+    
 ]
