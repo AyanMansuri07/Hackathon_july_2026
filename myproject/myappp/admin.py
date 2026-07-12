d2a17adf9500d4174c775dcdd2a6360b8ad82805
aaryannp <aryanparmar5116@gmail.com>
2026-07-12 17:14:59 +0530

    Remove everything except documentation per user request

--- myproject/myappp/admin.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-from .models import *
-
-admin.site.register(User)
-admin.site.register(Employee)
-admin.site.register(Department)
-admin.site.register(AssetCategory)
-admin.site.register(Asset)
-admin.site.register(AssetAllocation)
